1ce968a85c0fb05e011fef29f51d38431c9ec6c1
melowny <dani.hediger@gmail.com>
2023-10-03 12:32:09 +0200

    unocard

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-
+angular.module('Cards', ['ngSanitize'])
+    .directive('card', function () {
+        return {
+            restrict: 'E',
+            template: '<div ng-class="color">' +
+                '<div class="ellipse"></div>' +
+                '<div class="content" ng-class="color" ng-bind-html="content"></div>' +
+                '<div class="small-content" ng-class="color" ng-bind-html="smallContent"></div>' +
+                '<div class="small-content-reverse" ng-class="color" ng-bind-html="smallContent"></div>' +
+                '</div>',
+            scope: {
+                type: '@',
+                content: '@',
+                smallContent: '@',
+                color: '@'
+            },
+            link: function (scope, element, attrs, controller) {
+                if (scope.type === 'reverse') {
+                    scope.color = scope.color || 'black';
+                    scope.content = $('#reverse-template').html();
+                    scope.smallContent = scope.smallContent || scope.content;
+                }
+            }
+        };
+    });
